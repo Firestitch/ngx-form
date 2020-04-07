c7ab202bd124e10f051ef6eacc6ce97b4e06b285
Raymond Gigliotti <ray@firestitch.com>
2020-04-07 17:32:38 -0400

    removed element based on class query

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,3 @@
-import { FsSubmitButtonDirective } from './../../directives/submit-button.directive';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -19,13 +18,14 @@ import {
 import { NgForm, AbstractControl } from '@angular/forms';
 import { values } from 'lodash-es';
 import { FsForm } from '../../services/fsform.service';
-import { isObservable, Subject, fromEvent } from 'rxjs';
-import { takeUntil, first } from 'rxjs/operators';
+import { isObservable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { FsMessage, MessageMode } from '@firestitch/message';
 import { MatDialogRef } from '@angular/material/dialog';
 import { confirmUnsaved } from '../../helpers/confirm-unsaved';
 import { FsPrompt } from '@firestitch/prompt';
 import { FsFormDialogCloseDirective } from '../../directives/form-dialog-close.directive';
+import { FsSubmitButtonDirective } from './../../directives/submit-button.directive';
 import { guid } from '@firestitch/common';
 
 
@@ -97,7 +97,6 @@ export class FsFormComponent implements OnInit, OnDestroy, AfterContentInit {
 
   private _destroy$ = new Subject();
   private _registerControl;
-  private _dirty = null;
 
   constructor(private _form: FsForm,
               private _element: ElementRef,
@@ -110,10 +109,6 @@ export class FsFormComponent implements OnInit, OnDestroy, AfterContentInit {
 
     if (this.dirtyConfirm && this.dirtyConfirmDialog) {
       this._registerDirtyConfirmDialogBackdrop();
-    }
-
-    if (this.dirtySubmitButton) {
-      this._registerDirtySubmitButton();
     }
 
     if (!this.autocomplete) {
@@ -223,9 +218,7 @@ export class FsFormComponent implements OnInit, OnDestroy, AfterContentInit {
         promise.then(() => {
           this._completeSubmit('submit-success', this._getSuccessSvg());
           this.ngForm.control.markAsPristine();
-          if (this.dirtySubmitButton) {
-            this._updateDirtySubmitButtons();
-          }
+          this._updateDirtySubmitButtons();
         }).catch(() => {
           this._completeSubmit('submit-error', this._getErrorSvg());
         });
@@ -241,6 +234,10 @@ export class FsFormComponent implements OnInit, OnDestroy, AfterContentInit {
   public ngAfterContentInit() {
     if (this.dirtyConfirm && this.dirtyConfirmDialog) {
       this._registerDirtyConfirmDialogClose();
+    }
+
+    if (this.dirtySubmitButton) {
+      this._registerDirtySubmitButton();
     }
   }
 
@@ -281,16 +278,20 @@ export class FsFormComponent implements OnInit, OnDestroy, AfterContentInit {
 
   private _completeSubmit(cls, svg) {
 
-    let el;
     this._submitButtons.forEach(button => {
-      el = new DOMParser().parseFromString(svg, 'text/xml').firstChild;
+      const el = new DOMParser().parseFromString(svg, 'text/xml').firstChild;
       button.element.classList.add(cls);
       button.element.append(el);
     });
 
     setTimeout(() => {
       this._submitButtons.forEach(button => {
-        button.element.removeChild(el);
+
+        const el = button.element.querySelector('.svg-icon');
+        if (el) {
+          button.element.removeChild(el);
+        }
+
         button.element.classList.remove(cls);
       });
 
@@ -356,13 +357,21 @@ export class FsFormComponent implements OnInit, OnDestroy, AfterContentInit {
       takeUntil(this._destroy$)
     )
     .subscribe(this._updateDirtySubmitButtons.bind(this));
+
+    this._submitButtons.changes
+    .pipe(
+      takeUntil(this._destroy$)
+    )
+    .subscribe(() => {
+      this._updateDirtySubmitButtons();
+    });
   }
 
   private _updateDirtySubmitButtons() {
-    if (this._dirty !== this.ngForm.dirty) {
-      this._dirty = this.ngForm.dirty;
+
+    if (this.dirtySubmitButton) {
       this._submitButtons.forEach(item => {
-        if (this._dirty) {
+        if (this.ngForm.dirty) {
           item.enable();
         } else {
           item.disable();
@@ -372,14 +381,14 @@ export class FsFormComponent implements OnInit, OnDestroy, AfterContentInit {
   }
 
   private _getProgressSvg() {
-    return '<svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".5" cx="18" cy="18" r="18"/><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur=".7s" repeatCount="indefinite"/></path></g></g></svg>';
+    return '<svg class="svg-icon svg-icon-progress" width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".5" cx="18" cy="18" r="18"/><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur=".7s" repeatCount="indefinite"/></path></g></g></svg>';
   }
 
   private _getSuccessSvg() {
-    return '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512px" height="512px"><g><g><g><path d="M437.019,74.98C388.667,26.629,324.38,0,256,0C187.619,0,123.331,26.629,74.98,74.98C26.628,123.332,0,187.62,0,256 s26.628,132.667,74.98,181.019C123.332,485.371,187.619,512,256,512c68.38,0,132.667-26.629,181.019-74.981 C485.371,388.667,512,324.38,512,256S485.371,123.333,437.019,74.98z M256,482C131.383,482,30,380.617,30,256S131.383,30,256,30 s226,101.383,226,226S380.617,482,256,482z" data-original="#000000" class="active-path" data-old_color="#000000" fill="#FFFFFF"/></g></g><g><g><path d="M378.305,173.859c-5.857-5.856-15.355-5.856-21.212,0.001L224.634,306.319l-69.727-69.727 c-5.857-5.857-15.355-5.857-21.213,0c-5.858,5.857-5.858,15.355,0,21.213l80.333,80.333c2.929,2.929,6.768,4.393,10.606,4.393 c3.838,0,7.678-1.465,10.606-4.393l143.066-143.066C384.163,189.215,384.163,179.717,378.305,173.859z" data-original="#000000" class="active-path" data-old_color="#000000" fill="#FFFFFF"/></g></g></g> </svg>';
+    return '<svg class="svg-icon svg-icon-success" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512px" height="512px"><g><g><g><path d="M437.019,74.98C388.667,26.629,324.38,0,256,0C187.619,0,123.331,26.629,74.98,74.98C26.628,123.332,0,187.62,0,256 s26.628,132.667,74.98,181.019C123.332,485.371,187.619,512,256,512c68.38,0,132.667-26.629,181.019-74.981 C485.371,388.667,512,324.38,512,256S485.371,123.333,437.019,74.98z M256,482C131.383,482,30,380.617,30,256S131.383,30,256,30 s226,101.383,226,226S380.617,482,256,482z" data-original="#000000" data-old_color="#000000" fill="#FFFFFF"/></g></g><g><g><path d="M378.305,173.859c-5.857-5.856-15.355-5.856-21.212,0.001L224.634,306.319l-69.727-69.727 c-5.857-5.857-15.355-5.857-21.213,0c-5.858,5.857-5.858,15.355,0,21.213l80.333,80.333c2.929,2.929,6.768,4.393,10.606,4.393 c3.838,0,7.678-1.465,10.606-4.393l143.066-143.066C384.163,189.215,384.163,179.717,378.305,173.859z" data-original="#000000" data-old_color="#000000" fill="#FFFFFF"/></g></g></g> </svg>';
   }
 
   private _getErrorSvg() {
-    return '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="512px" height="512px" viewBox="0 0 16 16" class="hovered-paths"><g><path fill="#FFFFFF" d="M8 1c3.9 0 7 3.1 7 7s-3.1 7-7 7-7-3.1-7-7 3.1-7 7-7zM8 0c-4.4 0-8 3.6-8 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8v0z" data-original="#444444" class="hovered-path active-path" data-old_color="#444444"/><path fill="#FFFFFF" d="M12.2 10.8l-2.8-2.8 2.8-2.8-1.4-1.4-2.8 2.8-2.8-2.8-1.4 1.4 2.8 2.8-2.8 2.8 1.4 1.4 2.8-2.8 2.8 2.8z" data-original="#444444" class="hovered-path active-path" data-old_color="#444444"/></g> </svg>';
+    return '<svg class="svg-icon svg-icon-error" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="512px" height="512px" viewBox="0 0 16 16"><g><path fill="#FFFFFF" d="M8 1c3.9 0 7 3.1 7 7s-3.1 7-7 7-7-3.1-7-7 3.1-7 7-7zM8 0c-4.4 0-8 3.6-8 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8v0z" data-original="#444444" data-old_color="#444444"/><path fill="#FFFFFF" d="M12.2 10.8l-2.8-2.8 2.8-2.8-1.4-1.4-2.8 2.8-2.8-2.8-1.4 1.4 2.8 2.8-2.8 2.8 1.4 1.4 2.8-2.8 2.8 2.8z" data-original="#444444" data-old_color="#444444"/></g> </svg>';
   }
 }
