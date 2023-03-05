54a8d93f5327ec58d3fff3a5a9754ed001e41791
Raymond Gigliotti <ray@firestitch.com>
2023-03-05 10:39:21 -0500

    removed FsFormDirective

--- playground/app/components/examples/examples.component.ts
+++ playground/app/components/examples/examples.component.ts
@@ -1,20 +1,16 @@
 import { Component, ViewChild } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { FormDeactivate, FsFormDirective } from '@firestitch/form';
 import { DeactivateComponent } from '../deactivate/deactivate.component';
 
 @Component({
   templateUrl: 'examples.component.html'
 })
-export class ExamplesComponent implements FormDeactivate {
+export class ExamplesComponent {
 
   public config = environment;
 
   @ViewChild(DeactivateComponent)
   public deactivate: DeactivateComponent;
 
-  public getForm(): FsFormDirective  {
-    return this.deactivate.form;
-  }
 }
 
